a326640ebcf5f15acc23a2dd48ec09de8e1ad9f2
Michelle-M <MichelleMullett@users.noreply.github.com>
2019-02-12 14:00:22 -0300

    Suma ej 101, 102, 103

--- Generales/ejercicios-generales.js
+++ Generales/ejercicios-generales.js
@@ -3,10 +3,58 @@
 /*var asterisco = '*****';
 var filas = 0;
 
-for ( asterisco = '*****'; filas <= 5; filas++ ) {
+for ( asterisco = '*****'; filas <= 4; filas++ ) { //si pongo 5, en consola me muestra (6)*****
     console.log(asterisco);
     asterisco = asterisco;
 }*/
+
+//Ejercicio 101
+
+/*var cincoAsteriscos = '*****';
+var dosAsteriscos = '*   *';
+var filas = 0;
+
+for ( dosAsteriscos = '*   *'; filas <= 3; filas++ ) {
+    for ( cincoAsteriscos = '*****'; filas <= 0; filas++ ) {
+        console.log(cincoAsteriscos);
+        cincoAsteriscos = cincoAsteriscos;
+    }
+    console.log(dosAsteriscos);
+    dosAsteriscos = dosAsteriscos;
+} for ( cincoAsteriscos = '*****'; filas <= 4; filas += 1) {
+    console.log(cincoAsteriscos);
+    cincoAsteriscos = cincoAsteriscos;
+}*/
+
+//Ejercicio 102
+
+/*var ochoAsteriscos = '* * * * * * * *';
+var sieteAsteriscos = ' * * * * * * * ';
+var filas = 0;
+
+for ( ochoAsteriscos = '* * * * * * * *'; filas <= 1; filas += 2) {
+    for ( sieteAsteriscos = ' * * * * * * * '; filas <= 2; filas += 2) {
+        console.log(sieteAsteriscos);
+        sieteAsteriscos = sieteAsteriscos;
+    } 
+    console.log(ochoAsteriscos);
+    ochoAsteriscos = ochoAsteriscos;
+}*/
+
+//Ejercicio 103
+
+var asteriscoPrincipio = '*';
+var asteriscoResta = '';
+var filas = 0;
+var resta = 4;
+
+for ( asteriscoPrincipio = '*'; filas <= 4; filas++ ) {
+    console.log(asteriscoPrincipio);
+    asteriscoPrincipio = asteriscoPrincipio + '**';
+} for ( asteriscoResta = ''; filas > 0; filas-- ) {
+    console.log(asteriscoResta);
+    resta = filas - 2;
+}
 
 //Ejercicio 104
 //Partiendo del siguiente código, realizar la suma de todos los números pares que hay entre el valor guardado en la variable n y el valor guardado en m (incluyéndolo)
