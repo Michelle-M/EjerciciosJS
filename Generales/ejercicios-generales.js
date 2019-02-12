eb6994c4eeeccea4ef6c0eef7fb9bac700222333
Michelle-M <MichelleMullett@users.noreply.github.com>
2019-02-12 12:11:16 -0300

    Nueva carpeta

---
+++ Generales/ejercicios-generales.js
@@ -0,0 +1,31 @@
+//Ejercicio 100
+
+/*var asterisco = '*****';
+var filas = 0;
+
+for ( asterisco = '*****'; filas <= 5; filas++ ) {
+    console.log(asterisco);
+    asterisco = asterisco;
+}*/
+
+//Ejercicio 104
+//Partiendo del siguiente código, realizar la suma de todos los números pares que hay entre el valor guardado en la variable n y el valor guardado en m (incluyéndolo)
+
+/*10 + 12 = 22
+  22 + 14 = 36
+  36 + 16 = 52
+  52 + 18 = 70
+  70 + 20 = 90
+  90 + 22 = 112
+  112 + 24 = 136
+  136 + 26 = 162
+  n     m  = n + m
+
+var n = 10;
+var m = 12; //modifiqué el valor por el 12 que es el inicio / 26 pasa a ser el valor final donde termina la suma
+//var resultado = n + m;
+
+for ( n = 10; m <= 26; m += 2) {
+    console.log(n + m);
+    n = n + m;
+}*/
