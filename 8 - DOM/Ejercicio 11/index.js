b4874c11407fc3c6e9f9201acc12214c81ba6d78
Michelle Mullett <44821939+Michelle-M@users.noreply.github.com>
2019-03-26 20:56:38 -0300

    Ejercicio 11

---
+++ 8 - DOM/Ejercicio 11/index.js
@@ -0,0 +1,11 @@
+var titulo = document.querySelector('h1');
+
+titulo.style.color = 'red';
+titulo.style.fontWeight = 'normal';
+titulo.style.textDecoration = 'underline';
+
+var parrafo = document.querySelector('p');
+
+parrafo.style.color = 'green';
+parrafo.style.fontWeight = 'bold';
+parrafo.style.border = '1px solid black';
