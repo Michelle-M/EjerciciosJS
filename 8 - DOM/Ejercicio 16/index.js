dcab43462174e5710698a94354093c712e609fa8
Michelle Mullett <44821939+Michelle-M@users.noreply.github.com>
2019-04-02 16:55:46 -0300

    Resuelto

---
+++ 8 - DOM/Ejercicio 16/index.js
@@ -0,0 +1,16 @@
+var ul = document.querySelector('ul');
+var primero = ul.children.item(0);
+var segundo = ul.children.item(1);
+var tercero = ul.children.item(2);
+var cuarto = ul.children.item(3);
+var quinto = ul.children.item(4);
+var sexto = ul.children.item(5);
+var septimo = ul.children.item(6);
+
+ul.removeChild(primero);
+ul.removeChild(segundo);
+ul.removeChild(tercero);
+ul.removeChild(cuarto);
+ul.removeChild(quinto);
+ul.removeChild(sexto);
+ul.removeChild(septimo);
