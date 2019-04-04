467a2aa06c6a352abb25576718a33a0c672ce563
Michelle Mullett <44821939+Michelle-M@users.noreply.github.com>
2019-04-04 17:36:55 -0300

    Revisar

--- 8 - DOM/Ejercicio 17/index.js
+++ 8 - DOM/Ejercicio 17/index.js
@@ -1,5 +1,6 @@
 var ul = document.querySelector('ul');
+var li = ul.children.item(6);
 
 for ( var i = 0; i < ul.children.length; i-- ) {
-    ul.removeChild(ul.lastChild);
+    ul.removeChild(li);
 }
